1c7010535638dd5471b28b8ced2b14c073fe85dc
Daryliost <152762468+Daryliost@users.noreply.github.com>
2024-09-30 14:32:19 -0700

    Notes

---
+++ Python_project/Personal_expense.py
@@ -0,0 +1,7 @@
+#1.) Add expenses
+#2.) View expenses
+#3.) Categorize expenses
+#4.) Visualize Data
+#5.) Export data
+#The goal is to allow users to track expenses and categorize and visualize spending habits.
+
