d934c38742b543fbe3833e596f135905ebb22423
Daryliost <152762468+Daryliost@users.noreply.github.com>
2024-10-09 08:15:19 -0700

    v1

--- Python_project/name_random_selector.py
+++ Python_project/name_random_selector.py
@@ -1,15 +1,15 @@
 import random
 
-names: dict = []
+names: list = []
 def name_machine() -> None:
     while True:
         global names
-        name: str = input("Please input names, 'q' or 'quit' to quit: ")
+        name = input("Please input names, 'q' or 'quit' to quit: ")
         if name.strip().lower() in ['q','quit']:
             break
         else:
             names.append(name)
-            print(name,'Has been chosen!!!!!\n')
+            print(name,'\n')
 
 def pick_random_name() -> None:
     global names
@@ -18,16 +18,18 @@ def pick_random_name() -> None:
     else:
         print("No names to pick from.")
 
-def create_weird_name() -> None:
-    weird_name: list = []
-    alphabet: list = list('stinky')
-    for _ in alphabet:
-        weird_name.append(random.choice(alphabet))
-    print("Weird name:", ''.join(weird_name))
-        
+def create_weird_name() -> str:
+    global names
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    weird_names = []
+    for name in names:
+        weird_name = ''.join(random.choice(alphabet) for _ in name)
+        weird_names.append(weird_name)
+    return ' '.join(weird_names)
+    
 
 if __name__ == "__main__":
     name_machine()
     pick_random_name()
-    create_weird_name()
-    
+    weird_name: str = create_weird_name()
+    print(f"\nWeird name: {weird_name}")
