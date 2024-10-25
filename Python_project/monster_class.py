17e1f35bf45431600260c53a2d9de35bec729649
Daryliost <152762468+Daryliost@users.noreply.github.com>
2024-10-25 08:06:13 -0700

    V2

--- Python_project/monster_class.py
+++ Python_project/monster_class.py
@@ -11,20 +11,29 @@ class Monster:
         self.weapon_level = weapon_level
 
     def damage(self, life_damage):
-        life_damage = self.life - self.weapon_level
-        return life_damage
+        self.life -= life_damage
+        return self.life
+
+    def attack(self, other):
+        damage_dealt = self.weapon_level - other.armour_level
+        if damage_dealt > 0:
+            other.damage(damage_dealt)
+            return(damage_dealt)
+        else:
+            other.damage(1)
+            return(damage_dealt)
     
 
 
-ogre = Monster(monster = 'ogre', level = 50, category = 'giant', armour_level = 1, arm_durability = 100, arm_health_pool = 100, life = 100, weapon_level = 6, wep_durability = 100, wep_health_pool = 100)
+ogre = Monster(monster = 'ogre', level = 50, category = 'giant', armour_level = 1,life = 100, weapon_level = 6,)
 
-human = Monster(monster = 'Chad', level = 30, category = 'Humanoid', armour_level = 3, arm_durability = 100, arm_health_pool = 100, life = 100, weapon_level = 3, wep_durability = 100, wep_health_pool = 100)
+human = Monster(monster = 'Chad', level = 30, category = 'Humanoid', armour_level = 3, life = 100, weapon_level = 3,)
 
-bear = Monster(monster = 'Bear', level = 20, category = 'Mammal', armour_level = 0, arm_durability = 0, arm_health_pool = 100, life = 100, weapon_level = 3, wep_durability = 100, wep_health_pool = 100)
+bear = Monster(monster = 'Bear', level = 20, category = 'Mammal', armour_level = 0, life = 100, weapon_level = 3,)
 
-gorilla = Monster(monster = 'Harambe', level = 30, category = 'Mammal', armour_level = 3, arm_durability = 100, arm_health_pool = 100, life = 100, weapon_level = 3, wep_durability = 100, wep_health_pool = 100)
+gorilla = Monster(monster = 'Harambe', level = 30, category = 'Mammal', armour_level = 3, life = 100, weapon_level = 3,)
 
-garden_gnome = Monster(monster = 'Gnome', level = 100, category = 'Lawn Decor', armour_level = 6, arm_durability = 100, arm_health_pool = 100, life = 100, weapon_level = 6, wep_durability = 100, wep_health_pool = 100)
+garden_gnome = Monster(monster = 'Gnome', level = 100, category = 'Lawn Decor', armour_level = 6, life = 100, weapon_level = 6,)
 
 def main() -> None:
     monsters = {
@@ -38,14 +47,25 @@ def main() -> None:
     Fighter1 = input('choose your monster, "Ogre, Human, Bear, Gorilla, or Garden Gnome: ').strip().lower()
     Fighter2 = input('choose opposing monster, "Ogre, Human, Bear, Gorilla, or Garden Gnome: ').strip().lower()
 
-    if Fighter1 in ['ogre','human','bear','gorilla','garden','gnome','garden gnome','gardengnome']:
-        print(f'you have selected {Fighter1} and {Fighter2}')
-    else:
-        print('Invalid creature, please select another')
-    
-    Fighter1 = monsters[Fighter1]
-    Fighter2 = monsters[Fighter2]
+    if Fighter1 in monsters and Fighter2 in monsters:
+        fighter1 = monsters[Fighter1]
+        fighter2 = monsters[Fighter2]
+        
+        while fighter1.life > 0 and fighter2.life > 0:
 
+            fighter1.attack(fighter2)
+            print(f'{fighter1.monster} swings at {fighter2.monster} dealing {fighter1.attack} damage!')
+
+            if fighter2.life <= 0:
+                print(f'{fighter2.monster} is defeated! {fighter1.monster} is victorious!')
+                break
+            
+            fighter2.attack(fighter1)
+            print(f'{fighter2.monster} swings at {fighter2.monster} dealing {fighter2.monster} damage!')
+
+            if fighter1.life <= 0:
+                print(f'{fighter1.monster} is defeated! {fighter2.monster} is victorious!')
+        
 
 if __name__ == '__main__':
     main()
